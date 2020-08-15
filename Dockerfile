d17a7909d6426c9bd986334202832f3788d9045a
homer.wong <wyg_098123@163.com>
2020-08-15 11:41:29 +0800

    chore: database

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.18.2-slim
+FROM node:12.18.2
 
 WORKDIR /usr/app/node-for-enterprise
 COPY . .
--- src/services/shop.js
+++ src/services/shop.js
@@ -1,63 +1,44 @@
-const { keys } = require('methods')
 
-const memoryStorage = {
-  1001: { name: '良品铺子' },
-  1002: { name: '来伊份' },
-  1003: { name: '三只松鼠' },
-  1004: { name: '百草味' }
-}
-
-async function delay (ms = 200) {
-  // eslint-disable-next-line promise/param-names
-  await new Promise(r => setTimeout(r, ms))
-}
+const { Shop } = require('../models')
 
 class ShopService {
-  async init () {
-    await delay()
-  }
+  async init () { }
 
   async create ({ values }) {
-    await delay()
-
-    const id = String(1 + Object.keys(memoryStorage).reduce((m, id) => Math.max(m, id), -Infinity))
-    return { id, ...(memoryStorage[id] = values) }
+    return await Shop.create(values)
   }
 
   async find ({ id, pageIndex = 0, pageSize = 10 }) {
-    await delay()
-
     if (id) {
-      return [memoryStorage[id]].filter(Boolean)
+      return [await Shop.findByPk(id)]
     }
 
-    return Object.keys(memoryStorage)
-      .slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
-      .map(id => ({ id, ...memoryStorage[id] }))
+    return await Shop.findAll({
+      offset: pageIndex * pageSize,
+      limit: pageSize
+    })
   }
 
   async modify ({ id, values }) {
-    await delay()
-
-    const target = memoryStorage[id]
+    const target = await Shop.findByPk(id)
 
     if (!target) {
       return null
     }
 
-    return Object.assign(target, values)
+    Object.assign(target, values)
+    console.log(target)
+    return await target.save()
   }
 
   async remove ({ id }) {
-    await delay()
-
-    const target = memoryStorage[id]
+    const target = await Shop.findByPk(id)
 
     if (!target) {
       return false
     }
 
-    return delete memoryStorage[id]
+    return target.destroy()
   }
 }
 let service
